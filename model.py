c7d543b65c387f9b4021d426fee7131aa7a8f40b
MahmoudSelmy <mahmoudselmy06@gmail.com>
2018-06-12 21:18:29 +0200

    finish model architecture

--- model.py
+++ model.py
@@ -48,7 +48,49 @@ class Deep3d:
 
         # output
         self.fc8 = self.helber.add_fully_connected(input=self.relu7, name="pred5")
-
         self.pred5 = tf.reshape(self.fc8,shape=[-1,5,12,33])
 
-        
+        # Branch 4
+        self.bn_pool4 = self.helber.add_batch_normalization_layer(input=self.pool4,name='bn_pool4')
+        self.pred4 = self.helber.add_convolutional_layer(input=self.bn_pool4,name='pred4',padding=(1,1))
+        # Branch 3
+        self.bn_pool3 = self.helber.add_batch_normalization_layer(input=self.pool3,name='bn_pool3')
+        self.pred3 = self.helber.add_convolutional_layer(input=self.bn_pool3,name='pred3',padding=(1,1))
+        # Branch 2
+        self.bn_pool2 = self.helber.add_batch_normalization_layer(input=self.pool2,name='bn_pool2')
+        self.pred2 = self.helber.add_convolutional_layer(input=self.bn_pool2,name='pred2',padding=(1,1))
+        # Branch 1
+        self.bn_pool1 = self.helber.add_batch_normalization_layer(input=self.pool1,name='bn_pool1')
+        self.pred1 = self.helber.add_convolutional_layer(input=self.bn_pool1,name='pred1',padding=(1,1))
+
+        scale = 1
+        self.relu_pred1 = self.helper.add_activation_layer(input=self.pred1,name='relu_pred1')
+        self.deconv_pred1 = self.helber.add_deconvolutional_layer(input=self.relu_pred1,scale=scale,name='deconv_pred1')
+
+        scale *= 2
+        self.relu_pred2 = self.helper.add_activation_layer(input=self.pred2, name='relu_pred2')
+        self.deconv_pred2 = self.helber.add_deconvolutional_layer(input=self.relu_pred2, scale=scale,name='deconv_pred2')
+
+        scale *= 2
+        self.relu_pred3 = self.helper.add_activation_layer(input=self.pred3, name='relu_pred3')
+        self.deconv_pred3 = self.helber.add_deconvolutional_layer(input=self.relu_pred3, scale=scale,name='deconv_pred3')
+
+        scale *= 2
+        self.relu_pred4 = self.helper.add_activation_layer(input=self.pred4, name='relu_pred4')
+        self.deconv_pred4 = self.helber.add_deconvolutional_layer(input=self.relu_pred4, scale=scale,name='deconv_pred4')
+
+        scale *= 2
+        self.relu_pred5 = self.helper.add_activation_layer(input=self.pred5, name='relu_pred5')
+        self.deconv_pred5 = self.helber.add_deconvolutional_layer(input=self.relu_pred5, scale=scale,name='deconv_pred5')
+
+        # merge
+        self.feat = self.deconv_pred1 + self.deconv_pred2 + self.deconv_pred3 + self.deconv_pred4 + self.deconv_pred5
+        self.feat_act = self.helber.add_activation_layer(input=self.feat,name='feat_act')
+        scale = 2
+        self.deconv_predup = self.helber.add_deconvolutional_layer(input=self.feat_act, scale=scale,name='deconv_predup')
+        self.relu_predup = self.helber.add_activation_layer(input=self.deconv_predup,name='relu_predup')
+        self.conv_predup = self.helber.add_convolutional_layer(input=self.relu_predup, name="conv_predup", padding=(1, 1))
+
+        # selection
+        self.mask = tf.nn.softmax(self.conv_predup)
+        self.right_image = self.helper.add_selection_layer(masks=self.mask, left_image= input_rgb)
